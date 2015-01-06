b3651075db8c78cb9d1978fc49559c2321564b61
sOm2y <yyin888@aucklanduni.ac.nz>
2015-01-06 17:53:19 +1300

    v 2.2.1

--- home.php
+++ home.php
@@ -1,24 +1,4 @@
 
-<?php 
-
-if(isset($_POST['submit'])){
-    $to = "sam@som2y.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $message = $_POST['message'];
-  	echo $to+$name+$message;
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    //echo $to+$name+$message;
-    mail($to,$name,$message,$headers);
-//    mail($from,$name,$message); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-?>
-
-
 
 
 
@@ -60,6 +40,30 @@ if(isset($_POST['submit'])){
 		<script src="js/modernizr.custom.js"></script>
 	</head>
 	<body>
+	<?php 
+		if(isset($_POST['submit'])){
+		    $to = "voncreativenz@gmail.com"; // this is your Email address
+		    $from = $_POST['email']; // this is the sender's Email address
+		    $name = $_POST['name'];
+			$subject = "Form submission";
+		    $subject2 = "Copy of your form submission";
+		    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+		    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+		    $headers = "From:" . $from;
+		    $headers2 = "From:" . $to;
+		    mail($to,$subject,$message,$headers);
+		    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+
+
+		    echo "<center>
+		    	<div class='feedback'>
+		    	<strong>Success!</strong> Your message has been sent successfully.
+				</div></center>";
+		    // You can also use header('Location: thank_you.php'); to redirect to another page.
+		    }
+		?>
+
 		<div class="container" id="container">
 			
             <div class="grid" id="grid">
@@ -408,7 +412,7 @@ if(isset($_POST['submit'])){
               <div class="about_container">
                   
                       
-                     <form class="form-horizontal"  action="" method="post" enctype="text/plain">
+                     <form class="form-horizontal"  action="" method="post">
                        
 
                         
@@ -608,8 +612,10 @@ if(isset($_POST['submit'])){
 //            });
 
 
-
-                              
+			setTimeout(function() {
+			    $('.feedback').fadeOut('fast');
+			}, 3000); // <-- time in milliseconds
+			                	              
            
 
 		</script>
